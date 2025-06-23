6a0bb8043d713640456a14e5fd80df3de758e577
Miguel Duarte <miduarte@c1r10s3.42lisboa.com>
2025-06-23 17:25:25 +0100

    grah

--- Makefile
+++ Makefile
@@ -6,10 +6,37 @@
 #    By: miduarte <miduarte@student.42lisboa.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/06/23 11:53:39 by miduarte          #+#    #+#              #
-#    Updated: 2025/06/23 11:53:46 by miduarte         ###   ########.fr        #
+#    Updated: 2025/06/23 12:37:51 by miduarte         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-$(NAME): $(OBJ)
-	$(CC) $(OBJ) -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -o $(NAME)
+NAME    = so_long
 
+SRCS    = main.c map.c player.c render.c utils.c
+OBJS    = $(SRCS:.c=.o)
+
+CC      = cc
+CFLAGS  = -Wall -Wextra -Werror
+
+MLX_DIR = ./minilibx-linux
+MLX     = -L$(MLX_DIR) -lmlx -lXext -lX11 -lm -lz
+
+RM      = rm -f
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(MLX)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -I$(MLX_DIR) -c $< -o $@
+
+clean:
+	$(RM) $(OBJS)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- main.c
+++ main.c
@@ -6,18 +6,39 @@
 /*   By: miduarte <miduarte@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 14:28:15 by miduarte          #+#    #+#             */
-/*   Updated: 2025/06/23 11:51:46 by miduarte         ###   ########.fr       */
+/*   Updated: 2025/06/23 14:30:34 by miduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./minilibx-linux/mlx.h"
-#include "./minilibx-linux/mlx_int.h"
+#include "so_long.h"
+
+typedef struct s_data {
+	void *img;
+	char *adr;
+	int bits_per_pixel;
+	int line_length;
+	int endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->adr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.adr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
