4657b42768a5e4b1fc31d637ae1bdab26de98644
Miguel Duarte <miduarte@c1r9s7.42lisboa.com>
2025-06-24 15:59:37 +0100

    now alternates two sprites

--- main.c
+++ main.c
@@ -6,39 +6,70 @@
 /*   By: miduarte <miduarte@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 14:28:15 by miduarte          #+#    #+#             */
-/*   Updated: 2025/06/23 14:30:34 by miduarte         ###   ########.fr       */
+/*   Updated: 2025/06/24 15:57:26 by miduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-typedef struct s_data {
-	void *img;
-	char *adr;
-	int bits_per_pixel;
-	int line_length;
-	int endian;
-}	t_data;
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int	xclose(t_vars *vars)
 {
-	char	*dst;
+	mlx_destroy_window(vars->mlx, vars->win);
+	mlx_destroy_display(vars->mlx);
+	exit(0);
+}
 
-	dst = data->adr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+int handleKeypress(int keycode, t_vars *vars)
+{
+	if (keycode == XK_Escape)
+		xclose(vars);
+	return (0);
+}
+int	ft_render(t_vars *vars)
+{
+	// Alternate frame every xrenders (adjust for speed)
+	static int	count = 0;
+	if (++count >= 10000)
+	{
+		vars->frame = 1 - vars->frame; // swap between 0 and 1
+		count = 0;
+	}
+	vars->img.img = vars->sprites->idle[vars->frame];
+	mlx_put_image_to_window(vars->mlx, vars->win, vars->img.img, 1920 / 2, 1080 / 2);
+	return (0);
 }
 
 int	main(void)
 {
+	t_vars	vars;
+	t_imgs	idleSprites;
+	int		img_width;
+	int		img_height;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Silklong");
+	idleSprites.idle[0] = mlx_xpm_file_to_image(vars.mlx, "./assets/Hornet-idle0000.xpm", &img_width, &img_height);
+	idleSprites.idle[1] = mlx_xpm_file_to_image(vars.mlx, "./assets/Hornet-idle0001.xpm", &img_width, &img_height);
+
+	vars.sprites = &idleSprites;
+	vars.frame = 0;
+	vars.img.img = idleSprites.idle[0];
+
+	mlx_loop_hook(vars.mlx, ft_render, &vars);
+	mlx_hook(vars.win, KeyPress, KeyPressMask, handleKeypress, &vars); // KeyPress = 2, KeyPressMask = 1L<<0
+	mlx_loop(vars.mlx);
+	return (0);
+}
+
+/*
+int	main(void)
+{
 	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	void	*img;
+	char	*relative_path = "./test.xpm";
+	int		img_width;
+	int		img_height;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.adr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
-}
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+}*/
--- so_long.h
+++ so_long.h
@@ -6,9 +6,33 @@
 /*   By: miduarte <miduarte@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 14:28:59 by miduarte          #+#    #+#             */
-/*   Updated: 2025/06/23 11:51:24 by miduarte         ###   ########.fr       */
+/*   Updated: 2025/06/24 15:55:21 by miduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minilibx-linux/mlx.h"
 #include "./minilibx-linux/mlx_int.h"
+#include <X11/X.h>
+#include <X11/keysym.h>
+#include <stdlib.h>
+
+typedef struct s_imgs {
+	void *idle[2];
+}	t_imgs;
+
+typedef struct s_data {
+	void *img;
+	char *adr;
+	int bits_per_pixel;
+	int line_length;
+	int endian;
+}	t_data;
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+	t_data	img;
+	t_imgs  *sprites;
+	int		frame;
+}	t_vars;
+
